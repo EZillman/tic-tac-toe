2e4c3a2e693c1d7660e789b522733b5bb3b809d7
EZillman <evelina.zillman@medieinstitutet.se>
2023-05-29 16:36:16 +0200

    a start?

---
+++ src/components/PlayerSelect.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+import { IPlayerData } from '../models/IPlayerData';
+
+const playerData = ref<IPlayerData>({playerName: ""})
+
+const state = reactive({ player:'Player X'})
+
+const saveName = () => {
+    state.player = 'Player O:'
+
+    //save from input
+}
+
+const handleSubmit = () => {
+    console.log(playerData.value.playerName);
+    localStorage.setItem('playerName', playerData.value.playerName)
+}
+
+</script>
+
+<template>
+    <div>
+        <h1>{{ state.player }}</h1>
+        <form @submit.prevent="handleSubmit">
+            <input type="text" v-model="playerData.playerName">
+            <button @click="saveName">Save</button>
+        </form>
+
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,23 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+
 createApp(App).mount('#app')
+
+/*
+const routes = [
+    {path: '/', component: HomeView}, 
+    {path: '/game', component: GameView},
+]
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+})
+
+const app = Vue.createApp({})
+
+app.use(router)
+
+app.mount('#app')
+*/
